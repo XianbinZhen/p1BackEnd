3b3d8d96bdd4283968954991d39fa33b7e6d846a
XianbinZhen <62436049+XianbinZhen@users.noreply.github.com>
2021-03-04 11:15:17 -0500

    ...

--- src/main/java/dev/zhen/controller/LoginController.java
+++ src/main/java/dev/zhen/controller/LoginController.java
@@ -5,10 +5,13 @@ import dev.zhen.entities.Employee;
 import dev.zhen.services.EmployeeService;
 import dev.zhen.utils.JwtUtil;
 import io.javalin.http.Handler;
+import org.apache.log4j.Logger;
 
 public class LoginController {
 
     private EmployeeService employeeService;
+
+    Logger logger = Logger.getLogger(LoginController.class.getName());
 
     public LoginController(EmployeeService employeeService) {
         this.employeeService = employeeService;
@@ -27,13 +30,16 @@ public class LoginController {
 //            ctx.cookie("jwt", jwt, 3600 * 24);
                 ctx.result(jwt);
                 ctx.status(201);
+                logger.info("username: " + employee.getUsername() + " login.");
             } else {
                 ctx.result("Incorrect username or password");
                 ctx.status(400);
+                logger.error("Incorrect username or password! Body: " + body);
             }
         } else {
             ctx.result("Failed to generate jwt");
             ctx.status(400);
+            logger.error("Failed to generate jwt! Body: " + body);
         }
     };
 
